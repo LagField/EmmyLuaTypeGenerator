5cb9f02d3cf41ff8e62a588dafb108dcba7fb145
LagField <hezaizhou@163.com>
2019-05-11 14:32:23 +0800

    Initial commit

---
+++ Assets/EmmyTypeGenerator/Editor/Generator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace EmmyTypeGenerator
+{
+    public static class Generator
+    {
+        private static string TypeDefineFilePath
+        {
+            get { return Application.dataPath + "/EmmyTypeGenerator/EmmyTypeDefine"; }
+        }
+
+        public static List<Type> exportTypeList;
+
+        [MenuItem("Lua/EmmyTypeGenerate")]
+        public static void GenerateEmmyTypeFiles()
+        {
+
+            exportTypeList = new List<Type>();
+            for (int i = 0; i < CustomSettings.customTypeList.Length; i++)
+            {
+                exportTypeList.Add(CustomSettings.customTypeList[i].type);
+                
+            }
+        }
+    }
+}
