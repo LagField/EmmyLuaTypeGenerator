631062d6cdbf607aa0625dd834de04223e431825
LagField <hezaizhou@163.com>
2019-05-13 15:46:29 +0800

    测试代码

--- Assets/EmmyTypeGenerator/Lua/Test.lua
+++ Assets/EmmyTypeGenerator/Lua/Test.lua
@@ -1,5 +1,5 @@
-require('EmmyTypeDefine')
 require('ExportTypeGlobalVariables')
 
 UnityEngine_Debug.Log('hello world')
 local newGameobject = UnityEngine_GameObject.New('New GameObject From Lua')
+TestExportScript.PrintGameobject(newGameobject)
