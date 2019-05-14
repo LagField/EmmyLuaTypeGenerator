f1dcf89e71d05ca8a653f8d18a840240083e668b
LagField <hezaizhou@163.com>
2019-05-14 16:22:58 +0800

    readme

--- Assets/EmmyTypeGenerator/Lua/Test.lua
+++ Assets/EmmyTypeGenerator/Lua/Test.lua
@@ -8,23 +8,6 @@ TestExportScript.TestDelegate(System_Action_float(function(number)
     UnityEngine_Debug.Log('delegate print ' .. number)
 end), 100)
 
-
-
-TestExportScript.TestDelegate(system(), 100)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 TestExportScript.TestDelegate(TestExportScript_CustomDelegate(function(content)
     UnityEngine_Debug.Log('custom delegate ' .. content)
 end), 'yes')
